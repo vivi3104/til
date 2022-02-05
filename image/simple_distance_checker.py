e8bcaebafc50636c4a850cfe916b1d8503b96427
vivi3104 <27960408+vivi3104@users.noreply.github.com>
2022-02-05 18:52:57 +0900

    change to show original distance

--- image/simple_distance_checker.py
+++ image/simple_distance_checker.py
@@ -1,9 +1,6 @@
-from ctypes.wintypes import tagRECT
 import sys, os
 import cv2
-from math import sqrt, fsum
-from cv2 import EVENT_FLAG_ALTKEY
-from cv2 import EVENT_MBUTTONDBLCLK
+from math import sqrt, fsum, ceil
 import numpy as np
 
 def cv_imread(input_file, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
@@ -40,6 +37,7 @@ def process_mouse_event(event, x, y, flags, params):
     input_image   = params['input_image']
     window_name   = params['window_name']
     points        = params['points']
+    resize_ratio  = params['resize_ratio']
     output_image_path  = params['output_image_path']
     perpendicular_line_length  = params['perpendicular_line_length']
 
@@ -57,10 +55,6 @@ def process_mouse_event(event, x, y, flags, params):
             points.pop(-1)
     
     img = input_image.copy()
-    
-    h, w = img.shape[:2]
-    cv2.line(img, (x, 0), (x, h), (255, 0, 0), 1)
-    cv2.line(img, (0, y), (w, y), (255, 0, 0), 1)
 
     # draw points
     if len(points) > 0:
@@ -80,7 +74,7 @@ def process_mouse_event(event, x, y, flags, params):
             start_pt = (x + int(perpendicular_vector[0] *  perpendicular_line_length//2), y + int(perpendicular_vector[1] *  perpendicular_line_length//2))
             end_pt   = (x + int(perpendicular_vector[0] * -perpendicular_line_length//2), y + int(perpendicular_vector[1] * -perpendicular_line_length//2))
             cv2.line(img, start_pt, end_pt, (0, 255, 0), 1, lineType=cv2.LINE_AA)
-            cv2.putText(img, "{:.1f} pix".format(distance), center_pt, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(img, "{:.1f} pix".format(distance*resize_ratio), center_pt, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
 
     # draw lines for the clicked points
     nLines = len(points)//2
@@ -88,17 +82,25 @@ def process_mouse_event(event, x, y, flags, params):
     for lpt in lines:
         cv2.line(img, lpt[0], lpt[1], (0, 0, 255), 1, lineType=cv2.LINE_AA)
         distance = dist(lpt[0], lpt[1])
+        original_distance = distance * resize_ratio
         center_pt = getCenterPoint(lpt[0], lpt[1])
-        cv2.putText(img, "{:.1f} pix".format(distance), center_pt, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
-
-    cv2.putText(img, "({0}, {1})".format(x, y), (0, 15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1, cv2.LINE_AA)
-    cv2.imshow(window_name, img)
-
+        cv2.putText(img, "{:.1f} pix".format(original_distance), center_pt, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1, cv2.LINE_AA)
+    
+    # save image when CTRL + LEFT CLICK
     if event == cv2.EVENT_LBUTTONDOWN:
         if flags & cv2.EVENT_FLAG_CTRLKEY:
             cv_imwrite(output_image_path, img)
             cv2.putText(img, "saving image to: " + output_image_path, (0, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1, cv2.LINE_AA)
-            cv2.imshow(window_name, img)
+
+    # draw cross lines and x,y coordinate in original image scale 
+    h, w = img.shape[:2]
+    cv2.line(img, (x, 0), (x, h), (255, 0, 0), 1)
+    cv2.line(img, (0, y), (w, y), (255, 0, 0), 1)
+    original_x = ceil(x * resize_ratio)
+    original_y = ceil(y * resize_ratio)
+    cv2.putText(img, "({}, {})".format(original_x, original_y), (0, 15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+    cv2.imshow(window_name, img)
 
 def main():
     '''
@@ -121,18 +123,18 @@ def main():
     h = image.shape[0]
 
     # expect full HD physical display
-    h_ratio = 720/h
-    scale = h_ratio if h_ratio < 1 else 1
-    target_image = cv2.resize(image, dsize=None, fx=scale, fy=scale)
+    h_ratio = 720/float(h)
+    scale_down_factor = h_ratio if h_ratio < 1 else 1
+    target_image = cv2.resize(image, dsize=None, fx=scale_down_factor, fy=scale_down_factor)
 
     points = []
-    num_points = 100
     perpendicular_line_length = 30
     window_name = 'mouse_event'
     params = {
         "input_image"  : target_image,
         "window_name"  : window_name,
         "points"       : points,
+        "resize_ratio" : 1/scale_down_factor,
         "output_image_path" : output_image_path,
         "perpendicular_line_length" : perpendicular_line_length
     }
